c83b0e657b44b4182eba8cf7eb96a00f3dba276b
Miles Quive <hilario19931@hotmail.com>
2019-05-13 21:17:16 +0200

    Add project files.

---
+++ IKMDatabase/Entities/Identity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+
+namespace IKMDatabase.Entities
+{
+    public class Identity
+    {
+        [Required]
+        [Key]
+        public int id { get; set; }
+    }
+}
---
+++ IKMDatabase/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+
+namespace IKMDatabase.Entities
+{
+    public class User : Identity
+    {
+        [Required]
+      
+        public string userNumber { get; set; }
+        public String name { get; set; }
+    }
+}
